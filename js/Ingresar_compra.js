813c471928e90838e11a37bd4c6ab87844e9c212
Yeim <brielalica@gmail.com>
2021-08-23 07:05:25 -0400

    Actualizacion de mosulo gestion lote parte 3

--- js/Ingresar_compra.js
+++ js/Ingresar_compra.js
@@ -20,10 +20,82 @@ $(document).ready(function () {
         let cantidad = $('#cantidad').val();
         let vencimiento = $('#vencimiento').val();
         let precio_compra = $('#precio_compra').val();
-        let producto_array=producto_select2.split(' | ');
-        console.log(producto_array);
+
+        if (producto_select2 == null) {
+            $('#error').text('Elija un producto');
+            $('#noadd-prod').hide('slow');
+            $('#noadd-prod').show(1000);
+            $('#noadd-prod').hide(4000);
+        }
+        else {
+            if (codigo_lote == '') {
+                $('#error').text('Introduzca un código');
+                $('#noadd-prod').hide('slow');
+                $('#noadd-prod').show(1000);
+                $('#noadd-prod').hide(4000);
+            }
+            else {
+                if (cantidad == '') {
+                    $('#error').text('Ingrese una cantidad');
+                    $('#noadd-prod').hide('slow');
+                    $('#noadd-prod').show(1000);
+                    $('#noadd-prod').hide(4000);
+                }
+                else {
+                    if (vencimiento == '') {
+                        $('#error').text('Ingrese un vencimiento');
+                        $('#noadd-prod').hide('slow');
+                        $('#noadd-prod').show(1000);
+                        $('#noadd-prod').hide(4000);
+                    }
+                    else {
+                        if (precio_compra == '') {
+                            $('#error').text('Ingrese un precio de compra');
+                            $('#noadd-prod').hide('slow');
+                            $('#noadd-prod').show(1000);
+                            $('#noadd-prod').hide(4000);
+                        }
+                        else {
+                            let producto_array = producto_select2.split(' | ');
+                            let producto = {
+                                id: producto_array['0'],
+                                nombre: producto_select2,
+                                codigo: codigo_lote,
+                                cantidad: cantidad,
+                                vencimiento: vencimiento,
+                                precio_compra: precio_compra
+                            }
+                            let template = '';
+                            template = `
+                            <tr>
+                                <td>${producto.nombre}</td>
+                                <td>${producto.codigo}</td>
+                                <td>${producto.cantidad}</td>
+                                <td>${producto.vencimiento}</td>
+                                <td>${producto.precio_compra}</td>
+                                <td><button class="borrar-producto  btn btn-danger"><i class="fas fa-times-circle"></i></button></td>
+                            </tr>
+                            `;
+                            $('#registros_compra').append(template);
+                            $('#add-prod').hide('slow');
+                            $('#add-prod').show(1000);
+                            $('#add-prod').hide(4000);
+                            $('#producto').val('').trigger('change');
+                            $('#codigo_lote').val('');
+                            $('#cantidad').val('');
+                            $('#vencimiento').val('');
+                            $('#precio_compra').val('');
+                        }
+                    }
+                }
+            }
+        }
 
 
+    });
+    $(document).on('click', '.borrar-producto', (e) => {
+        let elemento = $(this)[0].activeElement.parentElement.parentElement;
+        elemento.remove();
+    });
 
-    })
 });
